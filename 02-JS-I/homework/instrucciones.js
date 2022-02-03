4c0b99f51b347092380e7464ecdfbd5aab3d5cc1
CristhianMeyer <hawk.meyer1098@gmail.com>
2022-02-03 10:57:55 -0500

    homework-conceptos básico

---
+++ 02-JS-I/homework/instrucciones.js
@@ -0,0 +1,5 @@
+variable: es un lugar de almacenamiento
+strings: es un tipo de dato (texto) que va entre comillas
+funciones: es un conjunto de instrucciones que realiza una tarea o calcula algun valor 
+declaraciones: son instrucciones, estan separadas por un punto y coma
+valoresbooleanos: es un tipo de dato, donde este puede ser verdadero o falso
